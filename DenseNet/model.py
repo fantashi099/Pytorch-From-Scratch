Fix double softmax between last layer and CrossEntropyLoss

DenseNet/model.py
@@ -23,7 +23,6 @@ class Model(nn.Module):
         self.fc1 = nn.Linear(self.in_channels, 4096)
         self.fc2 = nn.Linear(4096, 4096)
         self.fc3 = nn.Linear(4096, num_classes)
-        self.softmax = nn.Softmax(dim=1)
         self.relu = nn.ReLU()
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
@@ -57,4 +56,4 @@ class Model(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
-        return self.softmax(x)
+        return x

Inception/model.py
@@ -14,7 +14,6 @@ class Model(nn.Module):
 
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(2)
-        self.logsoftmax = nn.LogSoftmax()
         self.fc = nn.Linear(2200,10)
     
     def forward(self, x):
@@ -23,5 +22,5 @@ class Model(nn.Module):
         x = self.relu(self.maxpool(self.conv2(x)))
         x = self.incept2(x)
         x = torch.flatten(x, 1)
-        x = self.logsoftmax(self.fc(x))
+        x = self.fc(x)
         return x

ResNet/main.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     optimizer = torch.optim.SGD(model.parameters(),lr=0.01, momentum=0.5)
 
     h_acc, h_loss, h_val_loss = [], [], []
-    for epoch in range(1, 20):
+    for epoch in range(1, 10):
         loss = train(epoch)
         val_loss, val_acc  = test()
         h_acc.append(val_acc)

ResNet/model.py
@@ -27,7 +27,6 @@ class Model(nn.Module):
         self.fc1 = nn.Linear(512, 4096)
         self.fc2 = nn.Linear(4096, 4096)
         self.fc3 = nn.Linear(4096, num_classes)
-        self.softmax = nn.Softmax(dim=1)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
         nn.init.xavier_uniform_(self.fc1.weight)
@@ -48,4 +47,4 @@ class Model(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
-        return self.softmax(x)
+        return x